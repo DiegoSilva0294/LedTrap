5dce3b45192d8fbf7977ad3943afb28bdcc67a68
Diego <diegosilvacarreras@gmail.com>
2023-11-13 09:47:08 -0300

    segundo commit probando pantalla celular

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ function HomePage() {
       <Head>
         <title>LED-Trap</title>
         <meta name="description" content="Pagina oficial LED-Trap" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Bienvenido />
     </Fragment>
